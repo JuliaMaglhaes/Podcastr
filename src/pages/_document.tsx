faa712ad5801dd50d81d76d9e5f44fea34f95618
Julia Magalhães <juliamagalhaes28@outlook.com>
2021-04-22 23:13:38 -0300

    logo e titulo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,9 +4,11 @@ export default class MyDocument extends Document {
     render(){
         return(
             <Html>
+                <title>Podcastr</title>
                 <Head>
                     <link rel="preconnect" href="https://fonts.gstatic.com"/>
                     <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet"/>
+                    <link rel="shortcut icon" type="image/x-icon" href="favicon.png" />
                 </Head>
                 <body>
                     <Main />
